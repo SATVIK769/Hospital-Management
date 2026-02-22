d0f3a395226d8628b0e9e7ad77e1810494e37f8b
SATVIK GUPTA <156520515+SATVIK769@users.noreply.github.com>
2026-02-22 16:44:52 +0530

    Update API_BASE URL to production server

--- frontend/VerifyServicePaymentPage.jsx
+++ frontend/VerifyServicePaymentPage.jsx
@@ -2,7 +2,7 @@
 import React, { useEffect } from 'react'
 import { useLocation, useNavigate } from 'react-router-dom';
 
-const API_BASE = "http://localhost:4000";
+const API_BASE = "https://hospital-managementserver.onrender.com";
 const VerifyServicePaymentPage = () => {
     const location=useLocation();
     const navigate=useNavigate();
