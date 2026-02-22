714603532cbbd6bfbfc48547b65f337be6e4f783
SATVIK GUPTA <156520515+SATVIK769@users.noreply.github.com>
2026-02-22 16:07:01 +0530

    Use environment variable for server port

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ import serviceAppointmentRouter from './routes/serviceAppointmentRouter.js';
 
 
 const app = express();
-const port = 4000;
+const port = process.env.PORT || 4000;
 
 const allowedOrigins = [
     "https://hospital-managementfrontend.onrender.com",
